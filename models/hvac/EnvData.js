e7b876731d57288a8c3744db22fc128685fdec96
Aaron Litzenberg <alitz55@gmail.com>
2016-12-22 01:25:10 -0600

    changed envdata model to use double(4,1) to accomodate 100s

--- models/hvac/EnvData.js
+++ models/hvac/EnvData.js
@@ -1,11 +1,11 @@
 module.exports = function(sequelize, DataTypes) {
 	return sequelize.define('EnvData', {
 		temperature: {
-			type: DataTypes.DOUBLE(3,1)
+			type: DataTypes.DOUBLE(4,1)
 			,allowNull: true
 		}
 		,humidity: {
-			type: DataTypes.DOUBLE(3,1)
+			type: DataTypes.DOUBLE(4,1)
 			,allowNull: true
 		}
 	});
