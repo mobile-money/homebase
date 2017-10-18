2d3370e1da2bd182e2b1026cec749adaab7ace73
Aaron Litzenberg <alitz55@gmail.com>
2017-10-17 21:05:43 -0500

    upped sensor healthcheck warning to 61 minutes

--- routes/hvac/EnvData.js
+++ routes/hvac/EnvData.js
@@ -57,7 +57,7 @@ module.exports = function(app, EnvData, _, io) {
 	// Check for recent updates
 	app.get("/api/v1/hvac/envData/healthCheck", function(req, res) {
 		console.log("performing sensor health check");
-		EnvData.healthCheck(21).then(function(result) {
+		EnvData.healthCheck(61).then(function(result) {
 			console.log("result of health check: "+result);
 			res.json(result);
 		}).catch(function(error) {
