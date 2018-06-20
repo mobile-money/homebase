e373d14eeb3d7769256da19369d9b3d9c024e1ea
alitz55 <alitz55@users.noreply.github.com>
2018-06-19 21:24:50 -0500

    Delete aws.js

--- config/aws.js
+++
@@ -1,8 +0,0 @@
-aws = {
-    dynamodb: {
-        user: "db_user",
-        api_key: "AKIAJ5QL7K3GPXTQ4LPQ",
-        secret_key: "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"
-    }
-};
-module.exports = aws;
